d8d959a19f89d158c72a9178e3c897f2dc204201
Seph De Busser <seph.debusser@gmail.com>
2024-09-04 16:29:10 +0200

    Simplified data test

--- worlds/anodyne/test/TestData.py
+++ worlds/anodyne/test/TestData.py
@@ -1,9 +1,7 @@
-from . import AnodyneTestBase
+import unittest
 from ..Data import Locations,Regions
 
-class TestData(AnodyneTestBase):
-    run_default_tests = False
-
+class TestData(unittest.TestCase):
     def test_consistent_region_names(self):
         for loc_region in Locations.locations_by_region:
             self.assertIn(loc_region,Regions.all_regions,"Location region not a real region")
